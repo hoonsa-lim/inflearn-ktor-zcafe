8978ecec5cd02ff5b5f195fc87441b1c4a36dc6a
hslim <myohoon95@gmail.com>
2024-09-01 01:33:53 +0900

    전역 에러 핸들링 추가

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,13 +1,7 @@
 package com.example
 
-import com.example.config.configureDatabase
-import com.example.config.configureDependencyInjection
-import com.example.config.configureHttp
+import com.example.config.*
 import com.example.config.configureLogging
-import com.example.config.configureRouting
-import com.example.config.configureSecurity
-import com.example.config.configureSerialization
-import com.example.config.configureSession
 import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
@@ -22,5 +16,6 @@ fun Application.module() {
     configureSecurity()
     configureSerialization()
     configureRouting()
+    configureErrorHandling()
     configureLogging()
 }
